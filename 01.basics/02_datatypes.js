2ee337d8fb29af21db33497199459a31b31802ca
SK Maqshood <skmaqshood99@gmail.com>
2024-10-20 10:18:45 +0000

    Conversion of DataTypes

---
+++ 01.basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "SK Maqshood"
+let age = 18
+let isLoggedIN = true
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false'
+// null => standlone value
+// undefined =>
+// symbol => unique
+
+console.log(typeof "SK Maqshood") // undefined
+console.log(typeof "null") // obeject
---
+++ 01.basics/03_conversionoperation.js
@@ -0,0 +1,60 @@
+let score = "MAQ"
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+// "33"=> 33
+// "33abc"=> NaN
+// true => 1
+// false => 0
+
+let isLoggedIn = "MaQ"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false 
+// "MaQ" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//**************** Operations *************** 
+
+let value = 7
+let negValue = -3
+// console.log(negValue)
+
+/* console.log(3+3);
+console.log(3-3);
+console.log(3*3);
+console.log(3/3);
+console.log(2**4);
+console.log(10%2); */
+
+let str1 = "hii"
+let str2 = " MaQ"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log("1"+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
